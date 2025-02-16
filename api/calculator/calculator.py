c599825356621c3c6b206d2764017ca79ea5be59
SayefReyadh <reyadh@craftsmenltd.com>
2025-02-17 00:32:48 +0600

    refactor: remove unused power, modulus, and floor divide methods from calculator

--- api/calculator/calculator.py
+++ api/calculator/calculator.py
@@ -12,14 +12,3 @@ class Calculator:
         if y == 0:
             return 'Cannot divide by 0'
         return x * 1.0 / y
-
-    def power(x, y):
-        return x ** y
-
-    def modulus(x, y):
-        return x % y
-
-    def floor_divide(x, y):
-        if y == 0:
-            return 'Cannot divide by 0'
-        return x // y
